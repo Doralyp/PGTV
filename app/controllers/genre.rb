9f3b4ba732c83ba5201b4fd441e1fa0e685230bd
alextaber <alex.taber0@gmail.com>
2015-05-01 17:03:28 -0400

    Added views/controllers to channels and genres, and made actors view

---
+++ app/controllers/genre.rb
@@ -0,0 +1,10 @@
+get '/genres/index' do
+  genres = Genre.all
+  erb :'genres/index', locals: {genres: genres}
+end
+
+get '/genres/:id' do
+  genre = Genre.find(params[:id])
+  shows = Show.where(genre_id: params[:id])
+  erb :'genres/genre', locals: {genre: genre, shows: shows}
+end
