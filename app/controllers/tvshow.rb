ae28d6dfaf2be5710d826094e25a45499f7d82a5
Doraly Pantaleon & Sid Watal <pair+doralyp.sidwatal@devbootcamp.com>
2015-05-01 19:27:08 -0400

    working tvshows index and show routes, and top-nav

---
+++ app/controllers/tvshow.rb
@@ -0,0 +1,9 @@
+get "/shows" do
+  shows = Show.all
+  erb :"/tvshows/index", locals:{shows: shows}
+end
+
+get "/shows/:id" do
+  show = Show.find(params[:id])
+  erb :"/tvshows/tvshow", locals:{show: show}
+end
