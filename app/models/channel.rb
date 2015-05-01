70ea0678fe7ea7ca1c9fe600fb44f10766b9d98f
alextaber <alex.taber0@gmail.com>
2015-05-01 17:41:14 -0400

    Add validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,5 @@
 class Channel < ActiveRecord::Base
   belongs_to :show
+
+  validates :name, presence: true
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ActiveRecord::Base
   has_many :shows
+
+  validates :name, presence: true
 end
